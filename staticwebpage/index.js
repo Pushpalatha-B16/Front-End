39fee31b0af10f14c7236d3e5949324047a0ef77
Pushpalatha-B16 <pushpalathabalaji1234@gmail.com>
2025-10-13 15:00:26 +0530

    Add files via upload

---
+++ staticwebpage/index.js
@@ -0,0 +1,8 @@
+function sidebar(){
+    document.getElementById("sidenavbar").style.display="flex";
+    document.querySelector(".container1").style.display="none";
+}
+function closesidebar(){
+    document.getElementById("sidenavbar").style.display="none";
+    document.querySelector(".container1").style.display="flex";
+}
